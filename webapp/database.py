b69722321496e0b7de722961979e74b75f64c15d
trend-yen-chou <yen_chou@trendmicro.com>
2024-07-12 10:12:54 +0800

    update config

--- webapp/database.py
+++ webapp/database.py
@@ -17,6 +17,7 @@ Base = declarative_base()
 class Database:
 
     def __init__(self, db_url: str) -> None:
+        print(db_url)
         self._engine = create_engine(db_url, echo=True, pool_pre_ping=True)
         self._session_factory = scoped_session(
             sessionmaker(
